989548ebf13f7c5f6b382f9d9d4de610f0c5db5e
Tom Fardell <thomasfardell@outlook.com>
2026-04-13 23:40:55 +0100

    Add string comparison and concatenation methods

--- string.c
+++ string.c
@@ -1,5 +1,8 @@
 #include "string.h"
 
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 
 String string_init(U8 *str, U64 len) {
@@ -20,4 +23,64 @@ char *string_get_cstring(Arena *a, String str) {
   cstr[str.len] = '\0';
 
   return cstr;
+}
+
+bool string_equals(String s1, String s2) {
+  if (s1.len != s2.len) {
+    return false;
+  }
+
+  return (memcmp(s1.str, s2.str, s1.len) == 0);
+}
+
+String string_append(Arena *a, String str, String suffix) {
+  String result;
+  result.len = str.len + suffix.len;
+  result.str = arena_alloc(a, result.len, 1);
+
+  memcpy(result.str, str.str, str.len);
+  memcpy(result.str + str.len, suffix.str, suffix.len);
+
+  return result;
+}
+
+String string_prepend(Arena *a, String str, String prefix) {
+  return string_append(a, prefix, str);
+}
+
+String string_concat(Arena *a, U64 count, ...) {
+  va_list args;
+  va_start(args, count);
+
+  U64 strings_size = count * sizeof(String);
+  Arena string_arena = arena_init(strings_size);
+  String *strings = (String *)arena_alloc(&string_arena, strings_size, 8);
+
+  for (U64 i = 0; i < count; ++i) {
+    strings[i] = va_arg(args, String);
+  }
+
+  va_end(args);
+
+  String result = string_concat_arr(a, count, strings);
+
+  arena_free(&string_arena);
+
+  return result;
+}
+
+String string_concat_arr(Arena *a, U64 count, String *strings) {
+  String result = {0};
+
+  for (U64 i = 0; i < count; ++i) {
+    result.len += strings[i].len;
+  }
+
+  result.str = arena_alloc(a, result.len, 1);
+
+  for (U64 i = 0, pos = 0; i < count; pos += strings[i].len, ++i) {
+    memcpy(result.str + pos, strings[i].str, strings[i].len);
+  }
+
+  return result;
 }
--- string.h
+++ string.h
@@ -1,6 +1,9 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include <stdarg.h>
+#include <stdbool.h>
+
 #include "arena.h"
 #include "definitions.h"
 
@@ -9,17 +12,29 @@ typedef struct String {
   U64 len;
 } String;
 
+#define string_literal(cstr_lit) (String){(U8 *)cstr_lit, (sizeof cstr_lit) - 1}
+
 // Get a string given a memory location and length
 String string_init(U8 *str, U64 len);
-// Get the string starting at a start location (inclusive) and ending at an end locatin (exclusive)
+// Get the string between a start location (inclusive) and an end location (exclusive)
 String string_init_range(U8 *start, U8 *end);
 // Get the string equivalent of a given cstring
 String string_init_cstring(char *cstr);
 
-#define string_literal(cstr) (String){(U8 *)cstr, (sizeof cstr) - 1}
-
-// Get a pointer to the null-terminated equiavalent of a String
+// Get the null-terminated equiavalent of a String, alllocating the result on the given arena
 char *string_get_cstring(Arena *a, String str);
+
+// Check whether two strings are equal
+bool string_equals(String s1, String s2);
+
+// Get the result of prepending a given prefix to a string (allocated on a given arena)
+String string_prepend(Arena *a, String str, String prefix);
+// Get the result of appending a given suffix to a string (allocated on a given arena)
+String string_append(Arena *a, String str, String suffix);
+// Concatenate strings passed as variadic args
+String string_concat(Arena *a, U64 count, ...);
+// Concatenate an array of strings
+String string_concat_arr(Arena *a, U64 count, String *strings);
 
 #endif  // STRING_H
 
